7df9313629dd111728ba48b81039170d419b9221
JuanRMata <rodry.mata@gmail.com>
2024-03-05 16:48:26 -0600

    implementacion del archivo js

---
+++ index.js
@@ -0,0 +1,16 @@
+let resultadoGeneral = 76;
+const total = 100;
+let resultadoReaction = 80;
+let resultadoMemory = 92;
+let resultadoVerbal = 61;
+let resultadoVisual = 73;
+const totalHabilidades = 100;
+
+console.log(`El resultado general es: ${resultadoGeneral}`);
+console.log(`El resultado total es: ${total}`);
+console.log(`El resultado de reaction es: ${resultadoReaction}`);
+console.log(`El resultado de memory es: ${resultadoMemory}`);
+console.log(`El resultado de verbal es: ${resultadoVerbal}`);
+console.log(`El resultado de visual es: ${resultadoVisual}`);
+console.log(`El resultado total de habilidades es: ${totalHabilidades}`);
+
